0bdb5a962127c2e689499d15c4267e9ed45dbdb2
DIEGOA363 <dex363@gmail.com>
2019-06-20 01:21:10 -0400

    trained on 30 samples need to change resize, binarize standarize code

--- code/RotNet/regression.py
+++ code/RotNet/regression.py
@@ -6,8 +6,9 @@ for i in range(1):
     from keras.layers import Dense, Dropout, Flatten, Input
     from keras.layers import Conv2D, MaxPooling2D
     from keras.models import Model
-
-    from utils import angle_error_regression, RotNetDataGenerator, binarize_images
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from utils import angle_error_regression, RotNetDataGenerator, binarize_images, rotate
 
 # we don't need the labels indicating the digit value, so we only load the images
 path = 'C:/Users/Diego/Documents/MATLAB/JHU/HUR/asymmetricParticles/AsymParticles/code/RotNet/data/stock'
@@ -43,26 +44,46 @@ model.compile(loss=angle_error_regression,
               optimizer='adam')
 
 # training parameters
-batch_size = 128
-nb_epoch = 5
-
+BATCH_SIZE = 4
+nb_epoch = 50
 
 # callbacks
-checkpointer = ModelCheckpoint('test.h5', verbose=1, save_best_only=True, save_weights_only=True)
-
+checkpointer = ModelCheckpoint('code/RotNet/RotNet.h5', verbose=1, save_best_only=True, save_weights_only=True,monitor='loss')
+rot_generator = RotNetDataGenerator(
+    imgs,
+    input_shape = (43,43,1),
+    batch_size=BATCH_SIZE,
+    one_hot=False,
+    shuffle=True,
+    color_mode = 'grayscale',
+    preprocess_func=binarize_images
+)
 # training loop
 model.fit_generator(
-    RotNetDataGenerator(
-        imgs,
-        input_shape = (43,43,1),
-        batch_size=batch_size,
-        one_hot=False,
-        shuffle=True,
-        color_mode = 'grayscale',
-        preprocess_func=binarize_images
-    ),
-    steps_per_epoch=nb_train_samples / batch_size,
+    rot_generator,
+    steps_per_epoch=nb_train_samples / BATCH_SIZE,
     epochs=nb_epoch,
     verbose=1,
     callbacks=[checkpointer]
 )
+
+for i in range(1):
+    n_batches = 1
+
+    i = 0
+    im = np.zeros((nb_train_samples,)+(43,43)+(1,))
+    an = np.zeros(nb_train_samples)
+    for d, l in rot_generator:
+        im = d
+        an=l
+        i += 1
+        if i == n_batches:
+            break
+
+t = 2
+for i in range(1):
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(im[t,...].squeeze())
+    plt.subplot(122)
+    plt.imshow(rotate(im[t,...],-an[t]*360))
