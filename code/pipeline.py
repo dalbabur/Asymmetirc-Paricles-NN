e793a7304c4bc33e3d64b439eef38483034a8586
DIEGOA363 <dex363@gmail.com>
2019-07-01 19:51:49 -0400

    corrected pipeline/feed and added visualization of info

--- code/pipeline.py
+++ code/pipeline.py
@@ -18,22 +18,31 @@ def get_objects(y_pred, resize = None, min_size = 66,max_size=300):
         for j in range(classes-2): # don't care about background, don't care about UFOs
             bin = y_pred[i,:,:,j+1][:,:,np.newaxis].astype('uint8')
             contours = cv2.findContours(bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-            for k in range(len(contours[0])):
-                A = cv2.contourArea(contours[0][k])
-                if A > min_size and A < max_size:
-                    com = np.mean(cv2.findNonZero(cv2.drawContours(np.zeros_like(bin).astype('uint8'), contours[0], k, 255, -1)).squeeze(),axis=0))
-                    x,y,w,h = cv2.boundingRect((contours[0][k]))
-                    if x > 0:
-                        x = x-1
-                    if y > 0:
-                        y = y-1
-                    w = w+1
-                    h = h+1
-                    obj = bin[y:(y+h+1),x:(x+w+1)]
-                    if resize is not None:
-                        obj = cv2.resize(obj,resize)
-                    objects.append(obj)
-                    info.append([i,j,k,A,x,y,w,h,com])
+            cnts = contours[0]
+            if len(cnts)>0:
+                boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+                cnts, boundingBoxes = zip(*sorted(zip(cnts, boundingBoxes),key=lambda b:b[1][1], reverse=False))
+
+                for k in range(len(cnts)):
+                    A = cv2.contourArea(cnts[k])
+                    if A > min_size and A < max_size:
+                        com = np.mean(cv2.findNonZero(cv2.drawContours(np.zeros_like(bin).astype('uint8'), contours[0], k, 255, -1)).squeeze(),axis=0)
+                        if not isinstance(com,(np.ndarray,)):
+                            break
+                        x,y,w,h= boundingBoxes[k]
+                        if x > 0:
+                            x = x-1
+                        if y > 0:
+                            y = y-1
+                        w = w+1
+                        h = h+1
+                        obj = bin[y:(y+h+1),x:(x+w+1)]
+                        if resize is not None:
+                            obj = cv2.resize(obj,resize)
+                        objects.append(obj)
+                        info.append([i,j,k,A,x,y,w,h,com[0],com[1]])
+
     if resize is not None:
-        objects = np.array(objects)[:,:,:,np.newaxis]
-    return objects, np.array(info)
+        objects = np.array(objects)[...,np.newaxis]
+    info = np.array(info)
+    return objects, info
--- code/predict.py
+++ code/predict.py
@@ -4,7 +4,7 @@ for i in range(1):
     sys.path.insert(0, './code/UNET/')
     sys.path.insert(0, './code/RotNet/')
     sys.path.insert(0, './code')
-    from image import ImageDataGenerator, save_img
+    from image import ImageDataGenerator, save_img, to_categorical
     from utils import RotNetDataGenerator
     import matplotlib.pyplot as plt
     import cv2
@@ -38,41 +38,69 @@ predict_img_generator = predict_gen.flow_from_directory(
                 shuffle = False
 )
 
-unet_model = unet(pretrained_weights = 'code/UNET/UNET.h5', classes = classes)
+unet_model = unet(pretrained_weights = 'code/UNET/UNET_highW.h5', classes = classes)
 predictions = unet_model.predict_generator(predict_img_generator, steps = n_batches)
+final_masks = np.argmax(predictions,axis=-1)
 
 if save_masks:
- final_masks = np.argmax(predictions,axis=-1)
  file_names = listdir(from_path+'/frames/')
  if not os.path.exists(path+to_folder):
     os.makedirs(path+to_folder)
  for i in range(int(data_size)):
     save_img(path+to_folder+file_names[i],final_masks[i][:,:,np.newaxis]*255/classes)
 
-objects, info = pipeline.get_objects(predictions*255, resize = (32,32), min_size = 100, max_size = 150)
+new_input = to_categorical(final_masks,num_classes = classes)
+objects, info = pipeline.get_objects(new_input, resize = (32,32), min_size = 60, max_size = 150)
 objects.shape
-plt.hist(info[:,3])
 
-# info[np.argsort(info[:,3])][70]
-#
-# np.argmax(np.sort(info[:,3]) > 180)
-# plt.imshow(objects[np.argsort(info[:,3])][695,...].squeeze())
-# plt.imshow(p[2020,:,:,1].squeeze())
+for ii in range(1):
+    for i in [0,1,4,5]:
+        plt.figure()
+        plt.scatter(np.arange(0,len(objects)),info[:,i],marker='o',c = info[:,3])
+        plt.colorbar()
+    plt.figure()
+    plt.scatter(info[:,8],info[:,9],marker='o',c = info[:,3])
+    plt.colorbar()
 
-rotnet_model = rotnet(pretrained_weights = 'code/RotNet/RotNet.h5', classes = 360)
+    plt.figure(figsize = (16,8))
+    plt.scatter(info[:,8],info[:,9],marker='o',c = info[:,1],s = info[:,3])
+    plt.colorbar()
+
+    plt.figure()
+    [plt.hist(info[info[:,1] == d,3],alpha=0.75, label=d) for d in [0,1,2,3]]
+    plt.legend()
+
+    plt.figure()
+    plt.scatter(np.arange(0,len(objects)),info[:,3],marker='o',c = info[:,1])
+    plt.colorbar()
+
+
+
+plt.imshow(new_input[100,:,:,2].squeeze())
+
+
+rotnet_model = rotnet(pretrained_weights = 'code/RotNet/RotNet_wAugmentation.h5', classes = 360)
 
 # might have to break this into smaller batches
-angles = rotnet_model.predict_on_batch(objects[:1000,...]/255).squeeze()
+angles = rotnet_model.predict_on_batch(objects).squeeze()
 angles = np.argmax(angles,axis=-1)
 t = 2
-for i in range(10,20):
-    t=i
-    plt.figure()
-    plt.subplot(121)
-    plt.imshow(objects[t,...].squeeze(),cmap='gray')
-    plt.subplot(122)
-    plt.imshow(rotate(objects[t,...],-angles[t]),cmap='gray')
 
+import matplotlib.animation as animation
+fig = plt.figure()
+ims = []
+for i in range(1,274):
+    t=i
+    a1 = plt.subplot(121)
+    im1 = a1.imshow(objects[t,...].squeeze(),cmap='gray')
+    a2 = plt.subplot(122)
+    im2 = a2.imshow(rotate(objects[t,...],-angles[t]),cmap='gray')
+    ims.append([im1,im2])
+
+plt.rcParams['animation.ffmpeg_path'] = 'C:/Program Files (x86)/ffmpeg/bin/ffmpeg.exe'
+ani = animation.ArtistAnimation(fig, ims, interval=500)
+ani.save('code/rotations.avi')
+plt.show()
 
 for i in range(1):
     from image import array_to_img, img_to_array, load_img
@@ -114,3 +142,6 @@ for i in range(8):
     plt.imshow(objects[i,...].squeeze())
     plt.subplot(122)
     plt.imshow(rotate(objects[i,...],-angles[i]))
+
+
+# TODO: save data to excel
