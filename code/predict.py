8bab5dd4c249c16dbd934122d46856908c1c879a
DIEGOA363 <dex363@gmail.com>
2019-08-07 16:10:14 -0400

    organized inputs

--- code/predict.py
+++ code/predict.py
@@ -24,96 +24,89 @@ for i in range(1):
     pipeline = reload(pipeline)
     dv = reload(dv)
 
-movie = '/U/1466ul_min_1/'
-from_path = 'code/data/authentic'+movie
-save_masks = False
-data_size = 454
-BATCH_SIZE = 2
-classes = 2
-resize = (64,640)
-n_batches = data_size/BATCH_SIZE
-to_folder = '/data/predicted/mask/'+movie
 path = 'C:/Users/Diego/Documents/MATLAB/JHU/HUR/asymmetricParticles/AsymParticles/code'
+movie = '/L/1466ul_min_2/'
+from_path = 'code/data/authentic'+movie
+data_size = 454
+
+segmentation_resize = (64,640)
+classification_resize = (32,32)
+
+save_masks = False
+full_mask_folder = '/data/predicted/mask/'+movie
+save_objects_for_assisted_learning_classification = True
+particle_mask_folder = '/UNET/data/synthetic/particles/masks/'+movie[1:3]
+
+BATCH_SIZE = 2
+segmentation_classes = 2
+classification_classes = 3
+rotation_classes = 360
+
+segmentaion_weights = 'code/UNET/weights/UNET_bin.h5'
+classification_weights = 'code/ClassNet/weights/ClassNet4slim.h5'
+rotation_weights = 'code/RotNet/weights/RotNet_wNoise.h5'
+
+n_batches = data_size/BATCH_SIZE
 
 predict_gen = ImageDataGenerator(rescale = 1./255)
 predict_img_generator = predict_gen.flow_from_directory(
                 from_path,
-                target_size = resize,
+                target_size = segmentaion_resize,
                 color_mode = 'grayscale',
                 batch_size = BATCH_SIZE,
                 class_mode = None,
                 shuffle = False
 )
 
-unet_model = unet(pretrained_weights = 'code/UNET/weights/UNET_bin.h5', classes = classes)
+unet_model = unet(pretrained_weights = segmentaion_weights, classes = segmentation_classes)
 predictions = unet_model.predict_generator(predict_img_generator, steps = n_batches)
 final_masks = np.argmax(predictions,axis=-1)
-new_input = to_categorical(final_masks,num_classes = classes)
 new_input2 = (final_masks>0).astype(int);
 
-dv.display_segmentation(new_input2, random = False, frames = range(550,700))
+dv.display_segmentation(new_input2, random = False, frames = range(0,5))
 
 # for i in range(1):
 #
-    og = np.zeros(new_input2.shape)
-    i = 0
-    load_ogs = predict_gen.flow_from_directory(
-                    from_path,
-                    target_size = resize,
-                    color_mode = 'grayscale',
-                    batch_size = 1,
-                    class_mode = None,
-                    shuffle = False
-    )
+    # og = np.zeros(new_input2.shape)
+    # i = 0
+    # load_ogs = predict_gen.flow_from_directory(
+    #                 from_path,
+    #                 target_size = segmentation_resize,
+    #                 color_mode = 'grayscale',
+    #                 batch_size = 1,
+    #                 class_mode = None,
+    #                 shuffle = False
+    # )
+    #
+    # while i < data_size:
+    #     for d in load_ogs:
+    #         og[i,...] = d.squeeze()
+    #         i += 1
+    #         break
 
-    while i < data_size:
-        for d in load_ogs:
-            og[i,...] = d.squeeze()
-            i += 1
-            break
+    # start = 100
+    # n_batches = 20
+    # for t in range(start,start+n_batches*BATCH_SIZE):
+    #     # plt.figure(figsize=(16, 64))
+    #     # plt.imshow(og[t,...].squeeze())
+    #     # plt.plot(info[info[:,0]==t,9],info[info[:,0]==t,10],'r*')
+    #     # plt.plot(info[info[:,0]==t,5],info[info[:,0]==t,6],'g*')
+    #     plt.figure(figsize=(16, 64))
+    #     plt.imshow(final_masks[t,...])
+    #     plt.plot(info[info[:,0]==t,9],info[info[:,0]==t,10],'r*')
+    #     plt.plot(info[info[:,0]==t,5],info[info[:,0]==t,6],'g*')
+    #     plt.title(t)
 
-    start = 100
-    n_batches = 20
-    for t in range(start,start+n_batches*BATCH_SIZE):
-        # plt.figure(figsize=(16, 64))
-        # plt.imshow(og[t,...].squeeze())
-        # plt.plot(info[info[:,0]==t,9],info[info[:,0]==t,10],'r*')
-        # plt.plot(info[info[:,0]==t,5],info[info[:,0]==t,6],'g*')
-        plt.figure(figsize=(16, 64))
-        plt.imshow(final_masks[t,...])
-        plt.plot(info[info[:,0]==t,9],info[info[:,0]==t,10],'r*')
-        plt.plot(info[info[:,0]==t,5],info[info[:,0]==t,6],'g*')
-        plt.title(t)
-#
-# fig = plt.figure(figsize=(64,14))
-# ims = []
-# for i in range(len(d[:,0,0,0])):
-#     t=i+1
-#
-#     fig = plt.figure(figsize=(64,14))
-#
-#     a1 = plt.subplot(111)
-#     coms, = a1.plot(info[info[:,0]==t,9],info[info[:,0]==t,10],'r.',markersize=30)
-#     boxs, = a1.plot(info[info[:,0]==t,5],info[info[:,0]==t,6],'g.',markersize=30)
-#     im1 = a1.imshow(og[t,...].squeeze(),cmap='gray')
-#     plt.imshow(new_input2[1,...].squeeze())
-#     ims.append([im1,coms,boxs])
-#
-# plt.rcParams['animation.ffmpeg_path'] = 'C:/Program Files (x86)/ffmpeg/bin/ffmpeg.exe'
-# ani = animation.ArtistAnimation(fig, ims, interval=300)
-# ani.save('code/masks.avi')
-# plt.show()
-#
 # if save_masks:
 #  file_names = listdir(from_path+'/frames/')
-#  if not os.path.exists(path+to_folder):
-#     os.makedirs(path+to_folder)
+#  if not os.path.exists(path+full_mask_folder):
+#     os.makedirs(path+full_mask_folder)
 #  for i in range(int(data_size)):
-#     save_img(path+to_folder+file_names[i],final_masks[i][:,:,np.newaxis])
+#     save_img(path+full_mask_folder+file_names[i],final_masks[i][:,:,np.newaxis])
 
-cnn = classnet(classes = 3 ,pretrained_weights = 'code/ClassNet/weights/ClassNet4slim.h5',input_size = (32,32,1))
-rnn = rotnet(pretrained_weights = 'code/RotNet/weights/RotNet_wNoise.h5', classes = 360)
-objects, info = pipeline.get_objects(new_input2[0:500,...], cnn, rnn, resize = (32,32), min_size = 0, max_size = 1600)
+cnn = classnet(classes = classification_classes ,pretrained_weights = classification_weights,input_size = classification_resize + (1,) )
+rnn = rotnet(pretrained_weights = rotation_weights, classes = rotation_classes)
+objects, info = pipeline.get_objects(new_input2[0:500,...], cnn, rnn, resize = classification_resize, min_size = 0, max_size = 1600)
 
 dv.display_rotation(objects,random = True)
 
@@ -123,48 +116,27 @@ info.shape
 dv.object_summary(info)
 
 # FIND PARAMETERS
-for i in range(1):
-    distances = [75,100]
-    memories = [3,5]
-    total = 0
-    f1 = plt.figure(figsize = (32,32))
-
-    for j in range(len(memories)):
-        for k in range(len(distances)):
-            total = total+1
-            traj, labels, dists = pipeline.get_trajectories(info,distances[k],memories[j])
-            a1 = f1.add_subplot(len(memories),len(distances),total)
-            a1.title.set_text(len(traj))
-            for i in range(len(traj)):
-                particles = traj[i][:,2] != 2
-                a1.plot(traj[i][:,9],traj[i][:,10])
+# for i in range(1):
+#     distances = [75,100]
+#     memories = [3,5]
+#     total = 0
+#     f1 = plt.figure(figsize = (32,32))
+#
+#     for j in range(len(memories)):
+#         for k in range(len(distances)):
+#             total = total+1
+#             traj, labels, dists = pipeline.get_trajectories(info,distances[k],memories[j])
+#             a1 = f1.add_subplot(len(memories),len(distances),total)
+#             a1.title.set_text(len(traj))
+#             for i in range(len(traj)):
+#                 particles = traj[i][:,2] != 2
+#                 a1.plot(traj[i][:,9],traj[i][:,10])
 
 traj, labels, dists = pipeline.get_trajectories(info,65,3)
 
 
 
-#
-all_arad = all_a.copy()
-for i in range(len(all_arad)):
-    if all_arad[i] <= 90:
-        all_arad[i] =  -all_arad[i]*np.pi/180
-    if all_arad[i] < 270 and all_arad[i] > 90:
-        all_arad[i] = -((all_arad[i]-180))*np.pi/180
-    if all_arad[i] < 360 and all_arad[i] >= 270:
-        all_arad[i] = (90-all_arad[i]%90)*np.pi/180
 
-plt.plot(np.cos(all_arad),all_y)
-plt.plot(all_x[idx],all_arad[idx],'o')
-plt.plot(all_x,all_arad,'o')
-
-jeffery1 = lambda x,a,b,G: np.arctan(b/a*np.tan(a*b*G*(x)/(a**2+b**2)))
-SSE = lambda p: np.sum((all_arad-jeffery1(all_x,*p)**2))
-opti = optimize.differential_evolution(SSE, [[5,10],[5,15],[0,10]],seed=1)
-params, params_covariance = optimize.curve_fit(jeffery1, all_x, all_arad,p0 = opti.x,bounds = ([5,5,0],[10,15,10]))
-plt.plot(all_x[idx[17:-17]],(all_arad[idx[17:-17]]),'o')
-plt.plot(all_x[idx[17:-17]], jeffery1(all_x[idx[17:-17]],*params),'o')
-plt.plot(np.linspace(0,30,len(all_y)),jeffery1(np.linspace(0,30,len(all_y)),*params),'o')
-plt.plot(np.cos(jeffery1(np.linspace(0,30,len(all_y)),*params)),all_y)
 
 plt.figure(figsize = (24,12))
 for i in range(15):
@@ -178,11 +150,17 @@ ani = dv.traj_movie(new_input2,traj, frames = [550,720]) # TODO: implment not st
 ani.save('code/traj_U.avi')
 
 plt.figure(figsize = (24,12))
-signals = [10] #range(len(traj))
+signals = [10,11] #range(len(traj))
 for i in range(len(signals)):
     plt.plot(traj[signals[i]][5:-5,9],traj[signals[i]][5:-5,10],label = signals[i])
 plt.title('COM Tracking')
 plt.legend()
+
+if save_objects_for_assisted_learning_classification:
+
+
+
+
 
 for i in range(1):
     i = signals[np.argmax([len(traj[i][5:-5,11]) for i in signals])]
@@ -244,9 +222,34 @@ for i in range(1):
     plt.scatter(all_x,all_y,c= all_a, s = all_pa)
     plt.colorbar()
     plt.plot(all_x[idx], wave(all_x[idx],*params))
-plt.plot(all_x[idx],all_y[idx],'o--')
+
+plt.plot(all_x[idx],all_y[idx])
 fx = dv.fixed_comx(og,new_input2, traj[10][5:-5,:], figsize = (10,12), width = 40, markersize = 10)
 fx.save('code/test_fixed_comx_U.avi')
+
+# convert to [-90,90], then radians
+# all_arad = all_a.copy()
+# for i in range(len(all_arad)):
+#     if all_arad[i] <= 90:
+#         all_arad[i] =  -all_arad[i]*np.pi/180
+#     if all_arad[i] < 270 and all_arad[i] > 90:
+#         all_arad[i] = -((all_arad[i]-180))*np.pi/180
+#     if all_arad[i] < 360 and all_arad[i] >= 270:
+#         all_arad[i] = (90-all_arad[i]%90)*np.pi/180
+#
+# plt.plot(np.cos(all_arad),all_y)
+# plt.plot(all_x[idx],all_arad[idx],'o')
+# plt.plot(all_x,all_arad,'o')
+
+# fit jeffrey
+# jeffery1 = lambda x,a,b,G: np.arctan(b/a*np.tan(a*b*G*(x)/(a**2+b**2)))
+# SSE = lambda p: np.sum((all_arad-jeffery1(all_x,*p)**2))
+# opti = optimize.differential_evolution(SSE, [[5,10],[5,15],[0,10]],seed=1)
+# params, params_covariance = optimize.curve_fit(jeffery1, all_x, all_arad,p0 = opti.x,bounds = ([5,5,0],[10,15,10]))
+# plt.plot(all_x[idx[17:-17]],(all_arad[idx[17:-17]]),'o')
+# plt.plot(all_x[idx[17:-17]], jeffery1(all_x[idx[17:-17]],*params),'o')
+# plt.plot(np.linspace(0,30,len(all_y)),jeffery1(np.linspace(0,30,len(all_y)),*params),'o')
+# plt.plot(np.cos(jeffery1(np.linspace(0,30,len(all_y)),*params)),all_y)
 
 # fit fourier
 ######
@@ -276,8 +279,6 @@ fx.save('code/test_fixed_comx_U.avi')
 #
 # plt.scatter(all_x,all_y)
 # plt.plot(all_x, fit.model(x=all_x, **fit_result.params).y, color='green', ls=':')
-
-
 
 # color fitted curve by segments depending on angle
 ######
@@ -310,106 +311,3 @@ fx.save('code/test_fixed_comx_U.avi')
 # axs.set_xlim(x.min(), x.max())
 # axs.set_ylim(-1.1, 1.1)
 # plt.show()
-
-
-
-for i in range(50):
-    plt.figure()
-    plt.subplot(1,2,1)
-    plt.imshow(objects[i,:,:].squeeze())
-    plt.title([info[i,2],info[i,3]])
-    plt.subplot(1,2,2)
-    plt.imshow(rotate(objects[i,...].squeeze(),-angles[i]))
-    plt.title([-angles[i],info[i,9],info[i,10]])
-
-
-
-t = 2
-
-import matplotlib.animation as animation
-fig = plt.figure()
-ims = []
-for i in range(1,274):
-    t=i
-    a1 = plt.subplot(121)
-    im1 = a1.imshow(objects[t,...].squeeze(),cmap='gray')
-    a2 = plt.subplot(122)
-    im2 = a2.imshow(rotate(objects[t,...],-angles[t]),cmap='gray')
-    ims.append([im1,im2])
-
-plt.rcParams['animation.ffmpeg_path'] = 'C:/Program Files (x86)/ffmpeg/bin/ffmpeg.exe'
-ani = animation.ArtistAnimation(fig, ims, interval=500)
-ani.save('code/rotations.avi')
-plt.show()
-
-for i in range(1):
-    from image import array_to_img, img_to_array, load_img
-
-
-    mask = load_img('code/data/predicted/mask/U/1466ul_min_1/classification/classification8871.tif', color_mode = 'grayscale',target_size=(64,640))
-    mask = img_to_array(mask,dtype='uint8').squeeze()
-
-    info = list()
-    objects = list()
-    resize = (32,32)
-    min_size = 0
-    max_size = 1600
-    i = 20
-    bin = new_input2[i,:,:][:,:,np.newaxis].astype('uint8')
-    contours = cv2.findContours(bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cnts = contours[0]
-    if len(cnts)>0:
-        boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-        cnts, boundingBoxes = zip(*sorted(zip(cnts, boundingBoxes),key=lambda b:b[1][1], reverse=False))
-
-        for k in range(len(cnts)):
-            A = cv2.contourArea(cnts[k])
-            if A > min_size and A < max_size:
-                com = np.mean(cv2.findNonZero(cv2.drawContours(np.zeros_like(bin).astype('uint8'), contours[0], k, 255, -1)).squeeze(),axis=0)
-                if not isinstance(com,(np.ndarray,)):
-                    break
-                x,y,w,h= boundingBoxes[k]
-                if x > 0:
-                    x = x-1
-                if y > 0:
-                    y = y-1
-                w = w+1
-                h = h+1
-                obj = bin[y:(y+h+1),x:(x+w+1)]
-
-                if resize is not None:
-                    obj = cv2.resize(obj,resize)
-
-                objects.append(obj)
-                info.append([i,k,A,x,y,w,h,com[0],com[1]])
-
-    if resize is not None:
-        objects = np.array(objects)[:,:,:,np.newaxis]
-    info = np.array(info)
-
-pangles = rotnet_model.predict_on_batch(objects/255).squeeze()
-angles = np.argmax(angles, axis= -1)
-for i in range(100,150):
-    plt.figure()
-    plt.subplot(121)
-    plt.imshow(objects[i,...].squeeze())
-    plt.subplot(122)
-    plt.imshow(rotate(objects[i,...],-angles[i]))
-
-
-# TODO: save data to excel
-
-plt.hist(angles)
-plt.hist(info[:,0],n=1600)
-np.unique(info[:,0]).shape
-info[:,0].shape
-objects27, info27 = pipeline2.get_objects(new_input2[27:29,...], cnn, resize = (32,32), min_size = 0, max_size = 1600)
-
-plt.imshow(new_input2[27,...].squeeze())
-plt.plot(info27[info27[:,0]==1,9],info27[info27[:,0]==1,10],'r*')
-plt.plot(info27[info27[:,0]==1,5],info27[info27[:,0]==1,6],'g*')
-
-lame = [[1,2],[1],[[1,2,3],[1,23]]]
-lame[0][0] = [1,2,3,4]
-lame[:][-1]
-len(lame)
